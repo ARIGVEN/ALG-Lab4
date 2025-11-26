7dca5658a034d3f92eca68a0a05e3aac520a41dd
Kravtsov <maximkravtsov03@gmail.com>
2025-11-26 15:02:30 +0300

    first3

--- Lab4.cpp
+++ Lab4.cpp
@@ -5,67 +5,58 @@
 #include <map>
 #include <iomanip>
 
-class DiceRoller {
-private:
-    std::mt19937 gen;
+int dice(const std::string& specStr) {
+    static std::mt19937 gen(std::random_device{}());
+    int total = 0;
+    size_t start = 0;
     
-public:
-    DiceRoller() : gen(std::random_device{}()) {}
-    
-    int dice(const std::string& specStr) {
-        int total = 0;
-        size_t start = 0;
+    while (start < specStr.length()) {
+        size_t comma = specStr.find(',', start);
+        std::string dice = (comma == std::string::npos) ? 
+                           specStr.substr(start) : 
+                           specStr.substr(start, comma - start);
         
-        while (start < specStr.length()) {
-            size_t comma = specStr.find(',', start);
-            std::string token = (comma == std::string::npos) ? 
-                               specStr.substr(start) : 
-                               specStr.substr(start, comma - start);
-            
-            // Парсим количество дайсов
-            size_t d_pos = token.find('d');
-            if (d_pos == std::string::npos) continue;
-            
-            int count = 1;
-            if (d_pos > 0) {
-                count = std::stoi(token.substr(0, d_pos));
-            }
-            
-            // Парсим количество сторон и модификатор
-            int sides = 0, modifier = 0;
-            size_t plus = token.find('+', d_pos);
-            size_t minus = token.find('-', d_pos);
-            
-            if (plus != std::string::npos) {
-                sides = std::stoi(token.substr(d_pos + 1, plus - d_pos - 1));
-                modifier = std::stoi(token.substr(plus + 1));
-            } else if (minus != std::string::npos) {
-                sides = std::stoi(token.substr(d_pos + 1, minus - d_pos - 1));
-                modifier = -std::stoi(token.substr(minus + 1));
-            } else {
-                sides = std::stoi(token.substr(d_pos + 1));
-            }
-            
-            // Бросаем кости
-            std::uniform_int_distribution<int> dist(1, sides);
-            for (int i = 0; i < count; ++i) {
-                total += dist(gen);
-            }
-            total += modifier;
-            
-            start = (comma == std::string::npos) ? specStr.length() : comma + 1;
+   
+        size_t d_pos = dice.find('d');
+        if (d_pos == std::string::npos) continue;
+        
+        int count = 1;
+        if (d_pos > 0) {
+            count = std::stoi(dice.substr(0, d_pos));
         }
         
-        return total;
+        
+        int sides = 0, modifier = 0;
+        size_t plus = dice.find('+', d_pos);
+        size_t minus = dice.find('-', d_pos);
+        
+        if (plus != std::string::npos) {
+            sides = std::stoi(dice.substr(d_pos + 1, plus - d_pos - 1));
+            modifier = std::stoi(dice.substr(plus + 1));
+        } else if (minus != std::string::npos) {
+            sides = std::stoi(dice.substr(d_pos + 1, minus - d_pos - 1));
+            modifier = -std::stoi(dice.substr(minus + 1));
+        } else {
+            sides = std::stoi(dice.substr(d_pos + 1));
+        }
+        
+        
+        std::uniform_int_distribution<int> dice_drop(1, sides);
+        for (int i = 0; i < count; ++i) {
+            total += dice_drop(gen);
+        }
+        total += modifier;
+        
+        start = (comma == std::string::npos) ? specStr.length() : comma + 1;
     }
-};
+    
+    return total;
+}
 
 int main() {
-    DiceRoller roller;
-    
     // Пример использования
-    std::cout << "Пример броска: \"2d6+2,3d10\" = " 
-              << roller.dice("2d6+2,3d10") << "\n\n";
+    std::cout << "Пример броска: \"2d6,d10+3\" = " 
+              << dice("2d6,d10+3") << "\n\n";
     
     // Исследуем распределения
     std::vector<std::string> configs = {"1d6", "2d6", "3d6", "1d10", "2d10", "3d10"};
@@ -76,7 +67,7 @@ int main() {
         
         // Моделируем броски
         for (int i = 0; i < trials; ++i) {
-            int result = roller.dice(config);
+            int result = dice(config);
             freq[result]++;
         }
         
